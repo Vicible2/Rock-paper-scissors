179bf5995f7f5b62c1c698f55838a4264a457317
Manten Devillé <manten.d@hotmail.com>
2020-11-18 17:15:09 +0100

    Store value by clicking each button, not working yet

---
+++ js/rps.js
@@ -0,0 +1,21 @@
+(function () {
+
+   
+document.getElementById("rock").addEventListener("click", function() {
+
+    let keuze = "rock";
+})
+
+document.getElementById("paper").addEventListener("click", function() {
+
+    let keuze = "paper";
+})
+document.getElementById("scissors").addEventListener("click", function() {
+
+    let keuze = "scissors";
+    console.log(keuze);
+})
+
+
+
+});
