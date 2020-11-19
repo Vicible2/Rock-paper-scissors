34c529b4954ba6de8b7893a84c9851eba8dbeb94
Manten Devillé <manten.d@hotmail.com>
2020-11-19 09:45:01 +0100

    Add spacing and color difference on play on mobile for clarity

--- js/rps.js
+++ js/rps.js
@@ -1,8 +1,29 @@
 
 // three basic variables to select every individual button.
-let rockButton = document.querySelector('.rock');
+//identify document.querySelector('.rock) with variable so we don't have to type out this code every time we want to call the selected button.
+
+let rockButton = document.querySelector('.rock'); 
 let paperButton = document.querySelector('.paper');
 let scissorsButton = document.querySelector('.scissors');
 
 let yourPick = ""; // empty string to fill in later with players choice (rock/paper/scissors)
 //can also be written without string (let yourPick) but is for clarity
+
+
+
+//click function for every button
+rockButton.addEventListener('click', function (){
+    yourPick ="rock";
+    console.log(yourPick);
+})
+
+paperButton.addEventListener('click', function (){
+    yourPick ="paper";
+    console.log(yourPick);
+})
+
+scissorsButton.addEventListener('click', function (){
+    yourPick ="scissors";
+    console.log(yourPick);
+})
+
