25efb30502b884802ff7108f05cc46aaeb55f608
Manten Devillé <manten.d@hotmail.com>
2020-11-19 09:57:46 +0100

    Add random number generator for computerpick from choices array

--- js/rps.js
+++ js/rps.js
@@ -6,24 +6,44 @@ let rockButton = document.querySelector('.rock');
 let paperButton = document.querySelector('.paper');
 let scissorsButton = document.querySelector('.scissors');
 
+let choices = ['rock', 'paper', 'scissors']; // array for choices variables
+
+
 let yourPick = ""; // empty string to fill in later with players choice (rock/paper/scissors)
 //can also be written without string (let yourPick) but is for clarity
+let computerPick ="";
+//empty string for computerpick, will be filled in by index of choices array. -> index will be randomly generated.
 
 
 
 //click function for every button
 rockButton.addEventListener('click', function (){
+    let random = Math.floor(Math.random () * 3);
+
     yourPick ="rock";
-    console.log(yourPick);
+    computerPick = choices[random];
+
+    console.log("Your pick is " + yourPick);
+    console.log("The computer picked " + computerPick);
 })
 
 paperButton.addEventListener('click', function (){
+    let random = Math.floor(Math.random () * 3);
+
     yourPick ="paper";
-    console.log(yourPick);
+    computerPick = choices[random];
+
+    console.log("Your pick is " + yourPick);
+    console.log("The computer picked " + computerPick);
 })
 
 scissorsButton.addEventListener('click', function (){
+    let random = Math.floor(Math.random () * 3);
+
     yourPick ="scissors";
-    console.log(yourPick);
+    computerPick = choices[random];
+
+    console.log("Your pick is " + yourPick);
+    console.log("The computer picked " + computerPick);
 })
 
