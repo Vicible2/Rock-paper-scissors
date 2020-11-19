e4baf8628a05d92e8bf47d454cd573a968717f71
Manten Devillé <manten.d@hotmail.com>
2020-11-19 11:53:51 +0100

    Add if statements for yourPick vs computerPick
    Added if statements for every yourPick choice
    Moved Let announcement variable up to global level, add empty string to
    fill in later
    removed let result = document.... to just state document.queryselector
    to fill .result div with announcement text (changes on comparison)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let choiceButtons = document.querySelectorAll('.choice'); 
+let choiceButtons = document.querySelectorAll('.choice');
 // querySelectorAll = selecteren van alle elementen met gegeven class/ID
 let playButton = document.querySelector('.play');
 //querySelector = selecteren van specifieke class/id
@@ -11,20 +11,68 @@ let yourPick = "";
 let computerPick = "";
 //empty strings for clarity, also possible to just write: let computerPick, let yourPick
 
-choiceButtons.forEach((choice, function () { 
+let announcement = "";
+//empty string for announcement (will fill in empty HTML tag with result), see line 40 to 76
+
+
+//playerChoice
+
+choiceButtons.forEach(choice => {
     //forEach takes every member of choiceButtons individually, and defines each individual member as choice
     choice.addEventListener('click', function () {
         yourPick = choice.value;
         //yourPick will be defined as: the clicked(and previously defined) choice = button, its value (given in html), so rock paper or scissors
+        console.log("you picked " + yourPick);
     })
 
-}));
+});
+
+//computerChoice & comparison
 
 playButton.addEventListener('click', function () {
-    let random = Math.floor(Math.random () * 3);
-    // define generated number as random for future computerpick, this wil generate number between 0 and 2
-    computerPick = choices[random];
-    //choices[random] will select the index of the array 'choices', so 0 1 or 2, random has been defined above.
+            let random = Math.floor(Math.random() * 3);
+            // define generated number as random for future computerpick, this wil generate number between 0 and 2
+            computerPick = choices[random];
+            //choices[random] will select the index of the array 'choices', so 0 1 or 2, random has been defined above.
+            console.log("computer picked " + computerPick);
 
-    
-})
+
+            if (yourPick === "rock") {
+                if (computerPick === "paper") {
+                    announcement = "You Lost!";
+                }
+                if (computerPick === "scissors") {
+                    announcement = "You Won!";
+                }
+                if (computerPick === "rock") {
+                    announcement = "It's a TIE!";
+                }
+            }
+
+            if (yourPick === "paper") {
+                if (computerPick === "scissors") {
+                    announcement = "You Lost!";
+                }
+                if (computerPick === "rock") {
+                    announcement = "You Won!";
+                }
+                if (computerPick === "paper") {
+                    announcement = "It's a TIE!";
+                }
+            }
+
+            if (yourPick === "scissors") {
+                if (computerPick === "rock") {
+                    announcement = "You Lost!";
+                }
+                if (computerPick === "paper") {
+                    announcement = "You Won!";
+                }
+                if (computerPick === "scissors") {
+                    announcement = "It's a TIE!";
+                }
+            }
+            console.log(announcement);
+            document.querySelector('.result').innerHTML = announcement;
+
+            });
