bb923dd7cdc39336c3f040b4830fc08acec7dff3
Manten Devillé <manten.d@hotmail.com>
2020-11-19 09:16:46 +0100

    Add variable for players pick, add //comments for clarity

--- js/rps.js
+++ js/rps.js
@@ -1,3 +1,8 @@
+
+// three basic variables to select every individual button.
 let rockButton = document.querySelector('.rock');
 let paperButton = document.querySelector('.paper');
 let scissorsButton = document.querySelector('.scissors');
+
+let yourPick = ""; // empty string to fill in later with players choice (rock/paper/scissors)
+//can also be written without string (let yourPick) but is for clarity
